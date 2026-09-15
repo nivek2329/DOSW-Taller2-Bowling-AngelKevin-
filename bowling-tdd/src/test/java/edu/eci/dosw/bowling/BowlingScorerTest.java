92ce8385d5bc65d72ae0de48c337d6bce87ea667
kevin Angel <kevin.angel-a@mail.escuelaing.edu.co>
2026-09-15 11:04:12 -0500

    test: B4-B7 - bonos de strike, spares y juego perfecto

--- bowling-tdd/src/test/java/edu/eci/dosw/bowling/BowlingScorerTest.java
+++ bowling-tdd/src/test/java/edu/eci/dosw/bowling/BowlingScorerTest.java
@@ -14,6 +14,21 @@ class BowlingScorerTest {
         }
     }
 
+    private void rollPerfectGame(BowlingGame game) {
+        for (int i = 0; i < 12; i++) {
+            game.roll(10);
+        }
+    }
+
+   
+    private void rollAllSpares(BowlingGame game, int lastBonus) {
+        for (int i = 0; i < 10; i++) {
+            game.roll(5);
+            game.roll(5);
+        }
+        game.roll(lastBonus);
+    }
+
     @Test
     @DisplayName("score() antes de completar el juego lanza IllegalStateException")
     void scoreBeforeGameComplete_throwsException() {
@@ -58,5 +73,51 @@ class BowlingScorerTest {
         rollMany(game, 16, 0); 
 
         assertEquals(18, game.score());
+    }
+
+ 
+   
+
+    @Test
+    @DisplayName("Strike en frame 1 suma el bono de los dos tiros siguientes")
+    void strikeInFirstFrame_addsBonusFromNextTwoRolls() {
+        BowlingGame game = new BowlingGame();
+        game.roll(10); 
+        game.roll(4);
+        game.roll(3); 
+        rollMany(game, 16, 0); 
+
+        assertEquals(24, game.score());
+    }
+
+    @Test
+    @DisplayName("Dos strikes consecutivos seguidos de un tiro suman el bono correctamente")
+    void twoConsecutiveStrikes_thenRoll_addsBonusCorrectly() {
+        BowlingGame game = new BowlingGame();
+        game.roll(10); 
+        game.roll(10); 
+        game.roll(5);
+        game.roll(3); 
+        rollMany(game, 14, 0); 
+
+        assertEquals(51, game.score());
+    }
+
+    @Test
+    @DisplayName("Todos spares con ultimo tiro de bono anota 150")
+    void allSpares_withFinalBonusRoll_scores150() {
+        BowlingGame game = new BowlingGame();
+        rollAllSpares(game, 5);
+
+        assertEquals(150, game.score());
+    }
+
+    @Test
+    @DisplayName("Juego perfecto - 12 strikes - score debe ser 300")
+    void perfectGame_scores300() {
+        BowlingGame game = new BowlingGame();
+        rollPerfectGame(game);
+
+        assertEquals(300, game.score());
     }
 }
