33903b4fa0ba913e0a9b66e925032ec19b102d2a
kevin Angel <kevin.angel-a@mail.escuelaing.edu.co>
2026-09-15 10:47:08 -0500

    feat: GREEN - suma directa de todos los tiros del juego

--- bowling-tdd/src/main/java/edu/eci/dosw/bowling/BowlingScorer.java
+++ bowling-tdd/src/main/java/edu/eci/dosw/bowling/BowlingScorer.java
@@ -2,12 +2,15 @@ package edu.eci.dosw.bowling;
 
 import java.util.List;
 
-
 public class BowlingScorer {
 
-    /** Calcula el puntaje total dada la lista de frames de un juego. */
     public int calculate(List<Frame> frames) {
-        // TODO: implementar con TDD
-        return 0;
+        int total = 0;
+        for (Frame frame : frames) {
+            for (int roll : frame.getRolls()) {
+                total += roll;
+            }
+        }
+        return total;
     }
 }
