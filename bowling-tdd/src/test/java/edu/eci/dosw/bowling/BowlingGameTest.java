cbda5ed96b4ef314ab792e93d35990bd5ce94ec3
kevin Angel <kevin.angel-a@mail.escuelaing.edu.co>
2026-09-14 20:17:45 -0500

    test: RED - roll lanza excepcion con pines mayores a 10

--- bowling-tdd/src/test/java/edu/eci/dosw/bowling/BowlingGameTest.java
+++ bowling-tdd/src/test/java/edu/eci/dosw/bowling/BowlingGameTest.java
@@ -10,13 +10,22 @@ class BowlingGameTest {
     @Test
     @DisplayName("roll(-1) lanza IllegalArgumentException")
     void rollNegativePins_throwsException() {
-
         BowlingGame game = new BowlingGame();
-
 
         assertThrows(
             IllegalArgumentException.class,
             () -> game.roll(-1)
+        );
+    }
+
+    @Test
+    @DisplayName("roll(11) lanza IllegalArgumentException")
+    void rollMoreThanTenPins_throwsException() {
+        BowlingGame game = new BowlingGame();
+
+        assertThrows(
+            IllegalArgumentException.class,
+            () -> game.roll(11)
         );
     }
 }
