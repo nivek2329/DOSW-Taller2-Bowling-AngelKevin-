0cccf39df830466a692f7bc3fd9b6d7d3be0ff20
kevin Angel <kevin.angel-a@mail.escuelaing.edu.co>
2026-09-15 10:14:22 -0500

    chore: configura Maven (pom.xml), agrega FrameType y BowlingScorer, .gitignore

---
+++ bowling-tdd/src/main/java/edu/eci/dosw/bowling/BowlingScorer.java
@@ -0,0 +1,13 @@
+package edu.eci.dosw.bowling;
+
+import java.util.List;
+
+
+public class BowlingScorer {
+
+    /** Calcula el puntaje total dada la lista de frames de un juego. */
+    public int calculate(List<Frame> frames) {
+        // TODO: implementar con TDD
+        return 0;
+    }
+}
